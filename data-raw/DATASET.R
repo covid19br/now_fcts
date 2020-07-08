a993fd308e76556ab7c4b05d8f16766486e8310f
Andrea Sánchez Tapia <katori@gmail.com>
2020-07-08 16:26:17 -0700

    data geocode and drs

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,19 @@
 ## code to prepare `DATASET` dataset goes here
 geocode_ibge <- read.csv("./data-raw/geocode_ibge.csv")
+geocode_ibge$municipio.code <- sapply(geocode_ibge$id, function(x) substr(x, start = 1, stop = 6))
+
+#ainda nao se se precisa mesmo separar se já está lá
+micro.code   <- geocode_ibge$microrregiao.id
+meso.code    <- geocode_ibge$microrregiao.mesorregiao.id
+estado.code  <- geocode_ibge$microrregiao.mesorregiao.UF.id
+estado.sigla <- geocode_ibge$microrregiao.mesorregiao.UF.sigla
+
 DRS_SP <- read.csv("./data-raw/DRS_SP.csv")
-usethis::use_data(geocode_ibge, DRS_SP,internal = TRUE)
+usethis::use_data(geocode_ibge,
+                  DRS_SP,
+                  #micro.code,
+                  #meso.code,
+                  #estado.code,
+                  #estado.sigla,
+                  internal = TRUE)
 

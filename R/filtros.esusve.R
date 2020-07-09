4c934d52f2b0fd9737ccc0aa42592390a63482ab
saramortara <saramortara@gmail.com>
2020-07-09 12:30:53 -0300

    new parameter remove.col in filtros.

--- R/filtros.esusve.R
+++ R/filtros.esusve.R
@@ -2,19 +2,33 @@
 #'
 #' @param dados df
 #' @param tipo covid, srag, obitos_covid, obitos_srag
+#' @param remove.col Lógico. Se TRUE seleciona apenas colunas de datas usadas em nowcasting
 #'
 #' @importFrom dplyr filter select mutate
 #' @importFrom magrittr `%>%`
 #' @importFrom stringr str_detect
 #' @export
 #'
-filtros.esusve <- function(dados, tipo) {
+filtros.esusve <- function(dados, tipo, remove.col = TRUE) {
 
+  if (!tipo %in% "covid") {
+    stop("Tipo precisa ser covid")
+  }
+
+  # funcionando apenas para covid
   if (tipo == "covid")
     dados2 <- dados %>%
       filter(.data$resultadoteste == "Positivo" |
                stringr::str_detect(.data$classificacaofinal, "Confirma")) %>%
       select(.data$datainiciosintomas, .data$datanotificacao, .data$datateste, .data$dataencerramento) %>%
       mutate(dataencerramento = pmax(.data$datateste, .data$datanotificacao, .data$dataencerramento, na.rm = TRUE))
+
+  # seleciona apenas colunas usadas no nowcasting
+  if (remove.col) {
+    dados2 <- dados2 %>%
+      select(.data$datainiciosintomas, .data$datanotificacao, .data$datateste, .data$dataencerramento)
+  }
+
   return(dados2)
+
 }
--- R/filtros.sivep.R
+++ R/filtros.sivep.R
@@ -2,42 +2,61 @@
 #'
 #' @param dados df
 #' @param tipo covid, srag, obitos_covid, obitos_srag
-#' @param hospitalizados Logical. If TRUE filters only hosptalized cases
+#' @param hospitalizados Lógico. Se TRUE filtra apenas casos hospitalizados
+#' @param remove.col Lógico. Se TRUE seleciona apenas colunas de datas usadas em nowcasting
 #'
 #' @importFrom dplyr filter select mutate
-#' @export
+ #' @export
 #'
-filtros.sivep <- function(dados, tipo, hospitalizados) {
+filtros.sivep <- function(dados, tipo, hospitalizados, remove.col) {
+
   if (hospitalizados)
     dados <- dados %>% filter(hospital == 1)
 
-  if (tipo == "covid")
+  if (tipo == "covid") {
     dados2 <- dados %>%
-    filter(pcr_sars2 == 1 | classi_fin == 5) %>% #covid com nova classificacao
-    select(dt_notific, dt_sin_pri, dt_pcr, dt_digita) %>%
-    mutate(dt_pcr_dig = pmax(dt_pcr, dt_digita, dt_notific, na.rm = TRUE))
+      filter(pcr_sars2 == 1 | classi_fin == 5) %>% #covid com nova classificacao
+      mutate(dt_pcr_dig = pmax(dt_pcr, dt_digita, dt_notific, na.rm = TRUE))
+    if (remove.col) {
+      dados2 <- dados2 %>%
+        select(dt_notific, dt_sin_pri, dt_pcr, dt_digita)
+    }
+  }
 
-  if (tipo == "srag")
+  if (tipo == "srag") {
     dados2 <- dados %>%
-      select(dt_notific, dt_sin_pri, dt_digita) %>%
       mutate(dt_pcr_dig = pmax(dt_digita, dt_notific, na.rm = TRUE))
+    if (remove.col) {
+      dados2 <- dados2 %>%
+        select(dt_notific, dt_sin_pri, dt_digita)
+    }
+  }
 
-  if (tipo == "obitos.covid")
+  if (tipo == "obitos.covid") {
     dados2 <- dados %>%
       filter(pcr_sars2 == 1 | classi_fin == 5) %>% # covid com nova classificacao
       filter(evolucao == 2) %>%
       filter(!is.na(dt_evoluca)) %>%
       mutate(dt_encerra = pmax(dt_encerra, dt_digita, dt_evoluca,
-                               na.rm = TRUE)) %>%
-      select(dt_evoluca, dt_notific, dt_encerra)
+                               na.rm = TRUE))
+    if (remove.col) {
+      dados2 <- dados2 %>%
+        select(dt_evoluca, dt_notific, dt_encerra)
+    }
+  }
 
-  if (tipo == "obitos.srag")
+  if (tipo == "obitos.srag") {
     dados2 <- dados %>%
       filter(evolucao == 2) %>%
       filter(!is.na(dt_evoluca)) %>%
       mutate(dt_encerra = pmax(dt_encerra, dt_digita, dt_evoluca,
-                               na.rm = TRUE)) %>%
-      select(dt_evoluca, dt_notific, dt_encerra)
+                               na.rm = TRUE))
+    if (remove.col) {
+      dados2 <- dados2 %>%
+        select(dt_evoluca, dt_notific, dt_encerra)
+    }
+  }
 
   return(dados2)
+
 }
